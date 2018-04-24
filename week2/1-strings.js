ad20ce537afba9c126a3877dda1a388db752681e
amirno1 <amir.noshir@gmail.com>
2018-04-25 01:16:33 +0200

    homework week2 added

---
+++ week2/1-strings.js
@@ -0,0 +1,13 @@
+"use strict";
+
+let myString = "hello,this,is,a,difficult,to,read,sentence";
+
+console.log(myString);
+
+console.log("The length is :" + " " + myString.length);
+
+console.log(myString.replace(/,/g, " "));
+
+// string.replace (string , "new character")
+// g stands for global 
+
---
+++ week2/2-arrays.js
@@ -0,0 +1,26 @@
+"use strict";
+
+let favoriteAnimals = ["blowfish", "capricorn", "giraffe"];
+
+favoriteAnimals.push("turtle");
+
+console.log(favoriteAnimals);
+
+favoriteAnimals.splice(1,0, "meerkat");
+
+// array.splice (place item , remove item(s), "new item");
+
+console.log("The array would be : [ 'blowfish', 'meerkat', 'capricorn', 'giraffe', 'turtle' ]");
+
+
+console.log(favoriteAnimals);
+
+console.log("The array has a length of:" + favoriteAnimals.length);
+
+favoriteAnimals.splice(3,1);
+
+// deleting "giraffe" from the array
+
+console.log(favoriteAnimals);
+
+console.log("The item you are looking for is at index:" +" "+ favoriteAnimals.indexOf("meerkat"));
