aab352ad4dffbeed9cd25ba04c047ab2ef63c678
amirno1 <amir.noshir@gmail.com>
2018-04-25 01:37:56 +0200

    updated

--- week1/Hello-world.js
+++ week1/Hello-world.js
@@ -1,3 +1,7 @@
 "use strict";
 
+<<<<<<< HEAD
 console.log("Hello,world // English \nSalam,donya // Persian \nHallo,wereld // Dutch");
+=======
+console.log("Hello,world // English \nSalam,donya // Persian \nHallo,wereld // Dutch");
+>>>>>>> use strict added
--- week1/I am awesome.js
+++ week1/I am awesome.js
@@ -1,3 +1,7 @@
 "use strict";
 
+<<<<<<< HEAD
 console.log('i\'m awesome');
+=======
+console.log('i\'m awesome');
+>>>>>>> use strict added
