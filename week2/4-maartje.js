58a1d734cbf3eab8f2b0a311be4748ea736b6118
amirno1 <amir.noshir@gmail.com>
2018-04-30 17:17:43 +0200

    semicolon added

--- week2/4-maartje.js
+++ week2/4-maartje.js
@@ -1,61 +1,61 @@
 "use strict";
 
 const monday = [
-    {
-      name: 'Write a summary HTML/CSS',
-      duration: 180
-    },
-    {
-      name: 'Some web development',
-      duration: 120
-    },
-    {
-      name: 'Fix homework for class10',
-      duration: 20
-    },
-    {
-      name: 'Talk to a lot of people',
-      duration: 200
-    }
-  ];
-  
-  const tuesday = [
-    {
-      name: 'Keep writing summary',
-      duration: 240
-    },
-    {
-      name: 'Some more web development',
-      duration: 180
-    },
-    {
-      name: 'Staring out the window',
-      duration: 10
-    },
-    {
-      name: 'Talk to a lot of people',
-      duration: 200
-    },
-    {
-      name: 'Look at application assignments new students',
-      duration: 40
-    }
-  ];
-  
-  const tasks = monday.concat(tuesday);
-
-  let minutes = 0;
-
-  for (let i = 0; i < tasks.length; i++) {
-        if (tasks[i].duration >= 120) {
-            minutes = minutes + tasks[i].duration;
-        }
+  {
+    name: 'Write a summary HTML/CSS',
+    duration: 180
+  },
+  {
+    name: 'Some web development',
+    duration: 120
+  },
+  {
+    name: 'Fix homework for class10',
+    duration: 20
+  },
+  {
+    name: 'Talk to a lot of people',
+    duration: 200
   }
-  
-  let hours = minutes / 60 ;
+];
 
-  let perHourSalary = 20 ;
+const tuesday = [
+  {
+    name: 'Keep writing summary',
+    duration: 240
+  },
+  {
+    name: 'Some more web development',
+    duration: 180
+  },
+  {
+    name: 'Staring out the window',
+    duration: 10
+  },
+  {
+    name: 'Talk to a lot of people',
+    duration: 200
+  },
+  {
+    name: 'Look at application assignments new students',
+    duration: 40
+  }
+];
 
-  let theWholeSalary = hours * perHourSalary
+const tasks = monday.concat(tuesday);
 
-  console.log("Maartje has earned"+ " " +theWholeSalary.toFixed(2) + " "+ "€");
+let minutes = 0;
+
+for (let i = 0; i < tasks.length; i++) {
+  if (tasks[i].duration >= 120) {
+    minutes = minutes + tasks[i].duration;
+  }
+}
+
+let hours = minutes / 60;
+
+let perHourSalary = 20;
+
+let theWholeSalary = hours * perHourSalary;
+
+console.log("Maartje has earned" + " " + theWholeSalary.toFixed(2) + " " + "€");
