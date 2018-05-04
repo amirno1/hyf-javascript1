fa76ad6bcd1dc287fbe275a31c963665c9d4f8e7
amirno1 <amir.noshir@gmail.com>
2018-05-05 00:14:12 +0200

    step 8-more updated

--- week3/8-more.js
+++ week3/8-more.js
@@ -5,7 +5,7 @@ function advertisement() {
     //pop will remove the last element and we assign that removed element to lastElement
     const vehiclesPlural = vehicles.map(i => i + "s");
     //map will add a "s" at the end of every one of array elements
-    console.log("Amazing Amir's Garage, we service " + vehiclesPlural.toString().replace(/,/g, ", ") + " and " + lastElement + "s.");
+    console.log("Amazing Amir's Garage, we service " + vehiclesPlural.join().replace(/,/g, ", ") + " and " + lastElement + "s.");
     //toString convert the array to string and replace ,replaces the commas globally to ", "
 }
 
